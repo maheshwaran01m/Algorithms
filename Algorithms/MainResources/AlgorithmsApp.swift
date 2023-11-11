0b8cacc9fe2a5dc7b6ac8a0ec36a885a45b7d02f
MAHESHWARAN <maheshwaran@eam360.com>
2023-11-11 19:04:15 +0530

    first commit

---
+++ Algorithms/MainResources/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by MAHESHWARAN on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
