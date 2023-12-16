a9dffb17eef837e545ed1de5584cab9d97077af0
MAHESHWARAN <maheshwaran@eam360.com>
2023-12-16 19:02:56 +0530

    HigherOrder  - Filters

--- Algorithms/Swift-Algorithms/DataStructures/HigherOrder.playground/Contents.swift
+++ Algorithms/Swift-Algorithms/DataStructures/HigherOrder.playground/Contents.swift
@@ -18,5 +18,44 @@ extension Sequence {
 
 let numbers = [1, 2, 3].tranformed(Double.init)
 let numbers1 = [1: "One", 2: "Two", 3: "Three"].tranformed { "\($0.key) = \($0.value)"}
-print(numbers, numbers1)
+//print(numbers, numbers1)
 
+
+/// ``Filter``
+
+extension Sequence where Element: Equatable {
+  
+  func includes(_ search: Element) -> Bool {
+    for element in self where search == element {
+      return true
+    }
+    return false
+    
+//    includes { $0 == search }
+  }
+}
+
+extension Sequence {
+  
+  func includes(_ search: (Element) throws -> Bool) rethrows -> Bool {
+    for element in self where try search(element) {
+      return true
+    }
+    return false
+  }
+  
+  func satisfy(_ predicate: (Element) throws -> Bool) rethrows -> Bool {
+    try !contains { try !predicate($0) }
+  }
+  
+  func matching(_ predicate: (Element) throws -> Bool) rethrows -> [Element] {
+    var matches = [Element]()
+    
+    for element in self where try predicate(element) {
+      matches.append(element)
+    }
+    return matches
+  }
+}
+
+numbers.filter { $0 / 2 == 1 }
